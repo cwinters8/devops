f7d0e665502deebdd0bdc24601d0b796c6e887c4
cwinters8 <clwinters8@gmail.com>
2018-07-24 18:04:18 -0600

    s3fs boot script updates

--- aws/s3fs_boot_osx.sh
+++ aws/s3fs_boot_osx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Startup script for s3fs
+# Update PASSWD_FILE variable as needed
+
+PASSWD_FILE="/Users/clarkwinters/.passwd-s3fs"
 
 mkdir -p /root/startup
 
 echo "$(date +"%D %T") Starting s3fs..." > /root/startup/s3fs_boot.log
-/bin/s3fs mti-fileshare /s3-fileshare -o iam_role -o allow_other -o nonempty >> /root/startup/s3fs_boot.log
+/usr/local/bin/s3fs vbs-justice /s3-vbs-justice -o allow_other -o umask=0000 -o passwd_file=$PASSWD_FILE >> /root/startup/s3fs_boot.log
 
 if [ $? == 0 ]; then
     echo "$(date +"%D %T") s3fs started" >> /root/startup/s3fs_boot.log
